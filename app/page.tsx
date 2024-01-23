1b06ad83e81c2bfb7a9777c0009f29abbc1d0305
tarunkumarf22labs <tarun.kumar@f22labs.com>
2024-01-24 00:35:19 +0530

    going live

--- app/page.tsx
+++ app/page.tsx
@@ -13,25 +13,7 @@ const Tldraw = dynamic(
 );
 
 export default function Home() {
-  const [html, setHtml] = useState<null | string>(`<!DOCTYPE html>
-  <html lang="en">
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Hello World App</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-  </head>
-  <body class="bg-gray-800 flex items-center justify-center h-screen">
-    <div class="bg-gray-700 p-8 rounded-lg shadow-lg text-white">
-      <h1 class="text-3xl font-bold mb-4 text-center">Hello World</h1>
-      <div class="flex justify-center">
-        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
-          Click me
-        </button>
-      </div>
-    </div>
-  </body>
-  </html>`);
+  const [html, setHtml] = useState<null | string>(``);
 
   return (
     <>
